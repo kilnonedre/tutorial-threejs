135ee5eae17a5604224193275c43cbb4d7ebb4df
kilnonedre <1600513615@qq.com>
2024-03-27 16:39:17 +0800

    refactor: 文件位置修改

--- src/view/scrollAnimation/index.tsx
+++ src/view/scrollAnimation/index.tsx
@@ -1,13 +1,20 @@
 'use client'
 
 import React from 'react'
-import Experience from './component/experience'
+import { OrbitControls, ScrollControls } from '@react-three/drei'
+import Office from './component/office'
+import Overlay from './component/overlay'
 import { Canvas } from '@react-three/fiber'
 
 const ScrollAnimation = () => {
   return (
     <Canvas camera={{ fov: 64, position: [2.3, 1.5, 2.3] }}>
-      <Experience />
+      <ambientLight intensity={1} />
+      <OrbitControls enableZoom={false} />
+      <ScrollControls pages={3} damping={0.25}>
+        <Overlay />
+        <Office />
+      </ScrollControls>
     </Canvas>
   )
 }
